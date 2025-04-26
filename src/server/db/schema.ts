b9a53082f0d7004e037eda681584832670231b23
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-04-26 11:49:22 +0200

    adding the users table to the DB

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -11,3 +11,19 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 export const createTable = pgTableCreator((name) => `growcery_${name}`);
+
+export const users = createTable("users", (d) => ({
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  authProviderId: d.varchar({ length: 512 }),
+  full_name: d.varchar({ length: 256 }).notNull(),
+  email: d.varchar({ length: 512 }).notNull(),
+  functionId: d.integer(),
+  managerId: d.integer(),
+  orgUnitId: d.integer(),
+  lastLoginAt: d.timestamp({ withTimezone: true }),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+}));
