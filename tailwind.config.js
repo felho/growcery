0b9f48b3e6138ab1f74e9f0a4d497a423348609e
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-04-26 12:09:54 +0200

    oops, forgot to add this as well when dark mode was created

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
+  },
+  plugins: [],
+};
