b5aaea77d37e01c54e3f5f7a61c84d4eb3ea406b
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-04-27 09:56:08 +0200

    adding max width to main layout

--- src/app/_components/main-layout.tsx
+++ src/app/_components/main-layout.tsx
@@ -16,7 +16,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       <main
         className={`flex-1 overflow-auto p-4 transition-all duration-200 md:p-6 ${sidebarOpen ? "ml-64" : "ml-0"}`}
       >
-        {children}
+        <div className="mx-auto max-w-7xl">{children}</div>
       </main>
     </div>
   );
