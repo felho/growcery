5a6790b1a29587ff05cf4ef510d2323dcdc3e4f3
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-04-28 09:19:27 +0200

    finally, I like this color setting better, then the previous one

--- src/app/admin/_components/StatCard.tsx
+++ src/app/admin/_components/StatCard.tsx
@@ -30,7 +30,7 @@ const StatCard = ({
   return (
     <Card
       onClick={handleClick}
-      className={`stat-card border-border/50 border transition-all duration-300 ease-in-out ${route ? "hover:border-primary/20 group cursor-pointer hover:translate-y-[-2px] hover:shadow-[0_8px_16px_-6px_rgba(0,0,0,0.2)]" : ""}`}
+      className={`stat-card border-border border transition-all duration-300 ease-in-out ${route ? "hover:border-primary/20 group cursor-pointer hover:translate-y-[-2px] hover:shadow-[0_8px_16px_-6px_rgba(0,0,0,0.2)]" : ""}`}
     >
       <CardHeader className="flex flex-row items-center justify-between pb-2">
         <CardTitle className="text-muted-foreground group-hover:text-muted-foreground/80 text-sm font-medium">
