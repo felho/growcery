2cba427bca75544ef46ed92fd1b9978af1d5e243
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-04-28 20:03:11 +0200

    fixing a small issue with the main layout and removing an unnecessary div

--- src/app/_components/main-layout.tsx
+++ src/app/_components/main-layout.tsx
@@ -12,18 +12,16 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   };
 
   return (
-    <div className="bg-background text-foreground flex h-screen flex-col">
+    <div className="bg-background text-foreground flex min-h-screen flex-col">
       <TopNav toggleSidebar={toggleSidebar} hasSidebar={true} />
 
       <div className="flex flex-1 overflow-hidden">
-        <div>
-          <Sidebar isOpen={sidebarOpen} />
-          <main
-            className={`flex-1 overflow-auto p-4 transition-all duration-200 md:p-6 ${sidebarOpen ? "ml-64" : "ml-0"}`}
-          >
-            <div className="mx-auto max-w-7xl">{children}</div>
-          </main>
-        </div>
+        <Sidebar isOpen={sidebarOpen} />
+        <main
+          className={`flex-1 overflow-auto p-4 transition-all duration-200 md:p-6 ${sidebarOpen ? "ml-64" : "ml-0"}`}
+        >
+          <div className="mx-auto max-w-7xl">{children}</div>
+        </main>
       </div>
     </div>
   );
