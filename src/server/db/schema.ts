cd4aa55d7580055db53fc3db44ea36be2a1fac62
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-05-05 16:52:28 +0200

    for now, we drop the org-unit-function-managers table idea

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,9 +3,7 @@ export * from "./schema/tables/organizations";
 export * from "./schema/tables/org-email-domains";
 export * from "./schema/tables/functions";
 export * from "./schema/tables/org-units";
-export * from "./schema/tables/org-unit-function-managers";
 
 export * from "./schema/relations/users";
 export * from "./schema/relations/functions";
 export * from "./schema/relations/org-units";
-export * from "./schema/relations/org-unit-function-managers";
