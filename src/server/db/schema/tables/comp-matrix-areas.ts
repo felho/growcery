5b65f039abade7e0a8176517aacd31ae3a462153
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-05-14 07:12:26 +0200

    short description can be null

--- src/server/db/schema/tables/comp-matrix-areas.ts
+++ src/server/db/schema/tables/comp-matrix-areas.ts
@@ -5,7 +5,7 @@ import { compMatrices } from "./comp-matrices";
 export const compMatrixAreas = createTable("comp_matrix_areas", {
   id: bigint({ mode: "number" }).primaryKey().generatedByDefaultAsIdentity(),
   title: varchar({ length: 50 }).notNull(),
-  shortDescription: varchar({ length: 200 }).notNull(),
+  shortDescription: varchar({ length: 200 }),
   compMatrixId: bigint({ mode: "number" })
     .notNull()
     .references(() => compMatrices.id),
