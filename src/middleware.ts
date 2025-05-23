9003df38e270cb9841787702fb437b5960b60c3a
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-05-23 19:12:24 +0200

    feat: redirect unauthenticated users to our sign-in page in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/verify-email(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+  const { userId } = await auth();
+
+  if (!isPublicRoute(req) && !userId) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 });
 
