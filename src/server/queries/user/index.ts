b900cc497e72694717b0224a03e5cab7f5e887b4
Gergely Hodicska <gergely.hodicska@gmail.com>
2025-06-02 10:31:53 +0200

    creating a UserWithCalibrationData type for the comp matrix calibration page

--- src/server/queries/user/index.ts
+++ src/server/queries/user/index.ts
@@ -1,6 +1,8 @@
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { users } from "~/server/db/schema";
 import { userArchetypes } from "~/server/db/schema/tables/user-archetypes";
+import { orgUnits } from "~/server/db/schema/tables/org-units";
+import { compMatrixLevelAssessments } from "~/server/db/schema/tables/comp-matrix-level-assessments";
 
 // Base types
 export type User = InferSelectModel<typeof users>;
@@ -13,6 +15,12 @@ export type UserWithArchetype = User & {
 
 export type UserWithArchetypeAndAssignments = UserWithArchetype & {
   userCompMatrixAssignments: { id: number }[];
+};
+
+export type UserWithCalibrationData = User & {
+  archetype: InferSelectModel<typeof userArchetypes> | null;
+  orgUnit: InferSelectModel<typeof orgUnits> | null;
+  levelAssessments: InferSelectModel<typeof compMatrixLevelAssessments>[];
 };
 
 // Query exports
